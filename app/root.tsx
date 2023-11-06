9c3f3b40277f03acaf4caf633f1df5bd5abfa021
Diego Hernández Herrera <diegohh0411@gmail.com>
2023-11-05 21:05:32 -0600

    GA4 activado

--- app/root.tsx
+++ app/root.tsx
@@ -26,6 +26,23 @@ export default function App() {
         <Links />
       </head>
       <body className={`${isTheModalOpen? "overflow-hidden" : "" } bg-white font-sans`}>
+      <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-1N1DZPPJPE"
+            />
+      <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-1N1DZPPJPE');
+              `,
+              }}
+            />
         <Outlet context={[isTheModalOpen, setModal]} />
         <ScrollRestoration />
         <Scripts />
