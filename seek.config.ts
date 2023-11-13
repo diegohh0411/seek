bcab36325dc9d354795edad8016d511febd2fe43
Diego Hernández Herrera <diegohh0411@gmail.com>
2023-11-12 21:07:16 -0600

    v1

--- seek.config.ts
+++ seek.config.ts
@@ -1,12 +1,12 @@
 export const config = {
-    name: "Diego Hernández",
-    description: `Tengo 19 años; fundé el blog de apologética <a href="https://farodefe.org" target="_blank">Faro de Fe</a>, en el que explicamos, promovemos y defendemos la fe católica; y quiero ir a <a href="https://seek.focus.org" target="_blank">SEEK 24</a>: la mayor conferencia para universitarios católicos en Estados Unidos.`,
-    primaryColor: "37 99 235",
-    clabe: "646731258611084893",
-    bank: "Sistema de Transferencias y Pagos (STP)",
+    name: "María Emma",
+    description: `Tengo 19 años y quiero ir a <a href="https://seek.focus.org" target="_blank">SEEK 24</a>: la mayor conferencia para universitarios católicos en Estados Unidos.`,
+    primaryColor: "61 78 60",
+    clabe: "clabe de maria emma va aqui",
+    bank: "banco de maria emma va aqui",
     speiInstructions: "Puedes depositarme a mi cuenta personal. En la descripción de la transferencia, pon tu nombre y el texto 'SEEK' para poder agradecerte :)",
-    nombreDePasarela: "Mercado Pago",
-    urlDePasarela: "https://link.mercadopago.com.mx/diegohh",
+    //nombreDePasarela: "Mercado Pago",
+    //urlDePasarela: "https://link.mercadopago.com.mx/diegohh",
     ctaText: "¿Me ayudarías a vivirlo?",
-    workMessage: "¿Necesitas un blog profesional, una tienda en línea o un sitio web como este? ¡También soy desarrollador de código! <a href='https://wa.me/528131266343' target='_blank'>Trabajemos juntos.</a>"
+    //workMessage: "¿Necesitas un blog profesional, una tienda en línea o un sitio web como este? ¡También soy desarrollador de código! <a href='https://wa.me/528131266343' target='_blank'>Trabajemos juntos.</a>"
 }
